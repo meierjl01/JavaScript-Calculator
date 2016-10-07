0cc10bc54bcdd60bf6e0a3c0625a5200df364811
Jennifer Meier <meierjl01@gmail.com>
2016-10-07 16:42:50 -0500

    added math and alert

--- main.js
+++ main.js
@@ -6,13 +6,58 @@
 // var number2 = document.querySelector('.num2');
 // console.dir(number2);
 
-document.querySelector('.calc').onclick = function () {
-  // console.log('hi');
+document.querySelector('.plus').onclick = function() {
+    // console.log('hi');
+    var number1 = Number(document.querySelector('.num1').value);
+    var number2 = Number(document.querySelector('.num2').value);
+    var number3 = number1 + number2;
+    document.querySelector('.num3').value = number3;
 
-  var number1 = Number(document.querySelector('.num1').value);
-  var number2 = Number(document.querySelector('.num2').value);
-  var number3 = number1 + number2;
-  document.querySelector('.num3').value = number3;
-  console.log(number3);
+    if (isNaN(number3)) {
+        document.querySelector('.num3').value = alert('Not a valid number');
+        console.log('Not a valid number');
+    }
+    console.log(number3);
+};
 
+document.querySelector('.minus').onclick = function() {
+    // console.log('hi');
+    var number1 = Number(document.querySelector('.num1').value);
+    var number2 = Number(document.querySelector('.num2').value);
+    var number3 = number1 - number2;
+    document.querySelector('.num3').value = number3;
+
+    if (isNaN(number3)) {
+        document.querySelector('.num3').value = alert('Not a valid number');
+        console.log('Not a valid number');
+    }
+    console.log(number3);
+};
+
+document.querySelector('.divide').onclick = function() {
+    // console.log('hi');
+    var number1 = Number(document.querySelector('.num1').value);
+    var number2 = Number(document.querySelector('.num2').value);
+    var number3 = number1 / number2;
+    document.querySelector('.num3').value = number3;
+
+    if (isNaN(number3)) {
+        document.querySelector('.num3').value = alert('Not a valid number');
+        console.log('Not a valid number');
+    }
+    console.log(number3);
+};
+
+document.querySelector('.multiply').onclick = function() {
+    // console.log('hi');
+    var number1 = Number(document.querySelector('.num1').value);
+    var number2 = Number(document.querySelector('.num2').value);
+    var number3 = number1 * number2;
+    document.querySelector('.num3').value = number3;
+
+    if (isNaN(number3)) {
+        document.querySelector('.num3').value = alert('Not a valid number');
+        console.log('Not a valid number');
+    }
+    console.log(number3);
 };
